7533f2350f4a466217b607288e44d1b6cfb02cd3
pangestujava <hidayatr1234@gmail.com>
2024-04-25 17:08:09 +0700

    varible in golang

--- variable.go
+++ variable.go
@@ -3,9 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	// deklarasi variable dapat dilakukan menggunakan :=
 	name := "Nadia Aulia"
 	fmt.Println(name)
 
 	name = "Nadia Rahmah"
 	fmt.Println(name)
+
+	// variable multiple
+	var(
+		firstName = "Rahmat"
+		lastName = "Hidayat"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
 }
