7bbcc88b82f8baa6b028afd5122b6672cefdc233
pangestujava <hidayatr1234@gmail.com>
2024-04-25 17:29:28 +0700

    constant in go

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// const tidak dapat berubah berbeda dengan variable
+	// const juga tidak masalah jika tidak dipakai berbeda dengan variable
+	const (
+		firstName = "Rahmat"
+		lastName = "Hidayat"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
