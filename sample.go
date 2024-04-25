cffdebc54285de8f3301cffda6efb47e5a161ee9
pangestujava <hidayatr1234@gmail.com>
2024-04-25 17:00:28 +0700

    learn day 5

---
+++ sample.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Sample for main func in go!")
+}
---
+++ string.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Rahmat"))
+	fmt.Println("Rahmat Hidayat"[5])
+}
