2244acdb1dd42d923e9ab7ffd2dc0a2963d839d6
pangestujava <hidayatr1234@gmail.com>
2024-05-01 17:14:57 +0700

    array

---
+++ array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	names := []string{"Nadia", "Aulia", "Rahmah", "Rahmat"}
+	// names := [4]string{"Nadia", "Aulia", "Rahmah", "Rahmat"}
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+	fmt.Println(names[3])
+	fmt.Println(names)
+}
